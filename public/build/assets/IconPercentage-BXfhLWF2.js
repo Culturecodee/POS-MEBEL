f68889f31c74e73e1571b83118d9972692847d3c
M. Naufal Kuswandana <atbrxnk@gmail.com>
2026-07-06 20:04:54 +0700

    build

---
+++ public/build/assets/IconPercentage-BXfhLWF2.js
@@ -0,0 +1,6 @@
+import{c as e}from"./createReactComponent-T1h89Q6e.js";/**
+ * @license @tabler/icons-react v3.44.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M16 17a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-0"}],["path",{d:"M6 7a1 1 0 1 0 2 0a1 1 0 1 0 -2 0",key:"svg-1"}],["path",{d:"M6 18l12 -12",key:"svg-2"}]],o=e("outline","percentage","Percentage",a);export{o as I};
